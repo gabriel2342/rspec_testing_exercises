4c99c248901d2dbcdfdef114345ac11feb2e9c13
gabriel2342 <gabriel.pendas@gmail.com>
2022-06-11 13:56:52 -0600

    Practiced abstraction exercises up to 12

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:dog) { described_class.new('Rocky', 'Shepard', 'black') }
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when Dog has method name shared with other classes' do
+    include_examples 'shared method name'
+  end    
+  
+  # remove the 'x' before running this test
+  it 'is not barking' do
+    expect(dog.barking?).to eq(false)
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
-  end
-
-  # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(dog.sleeping?).to eq(true)
   end
 end
